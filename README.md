57443d63f1cc9ef6baadb950b3c7f2e8611f44f6
Adam4ever <adam4ever@gmail.com>
2023-01-05 09:31:20 -0400

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# rafflebox-jquery-script
-This is a small script which uses jQuery to retrieve jackpot information from the api. It displays that information in predefined element IDs.
+# Rafflebox API Jackpot Script
+
+This jQuery script retrieves event information from the Rafflebox API.
+
+The `index.html` file provided will showcases the IDs in use and will display the proper data if used in a server environment or by using a tool like `npx http-server`.
+
+It will format the data and inject it into predefined HTML element IDs.
+
+The jackpot total - `#jackpot-total`  
+The jackpot for customers (total / 2) - `#jackpot-half`  
+The formatted Draw date - `#drawdate`  
+Additionaly, any element with a `.rb-checkout` class will have it's href updated to the appropriate checkout link.
+
+## Recent Purchasers
+
+There are 4 recent purchasers who each have 3 values, (name, value, date) purchased and are associated to their elements in the following pattern.  
+`#recent-{#}-name`  
+`#recent-{#}-value`  
+`#recent-{#}-date`
---
+++ rb-event-data.js
@@ -0,0 +1,99 @@
+$.noConflict();
+
+jQuery(document).ready(function () {
+  // Configuration
+  var eventID;
+  var raffleShortlink = 'gralaska';
+
+  if (raffleShortlink) {
+    // Retrieve Event information
+    jQuery
+      .get(
+        'https://apic.rafflebox.ca/event-service/v2/events/' + raffleShortlink,
+        function (data) {
+          // Get JSON
+          var event = data.data.data;
+
+          // Format data
+          eventID = event.id;
+          var eventName = event.name;
+          var jackpot = '$' + event.jackpotCents / 100;
+          var jackpotHalf = '$' + event.jackpotCents / 100 / 2;
+          var drawDate = new Date(event.drawDate).toLocaleDateString('en-US', {
+            dateStyle: 'long',
+          });
+          var checkoutLink =
+            'https://checkout.rafflebox.us/goldrush?eventId=' +
+            eventID +
+            '&locale=en';
+
+          // Inject data
+          jQuery('#eventName').text(eventName);
+          jQuery('#jackpot-total').text(jackpot);
+          jQuery('#jackpot-half').text(jackpotHalf);
+          jQuery('#draw-date').text(drawDate);
+          jQuery('#eventId').text(eventID);
+          jQuery('.rb-checkout').attr('href', checkoutLink);
+        }
+      )
+      // Then retrieve Recent Purchasers
+      .done(function (data) {
+        if (eventID) {
+          jQuery.get(
+            'https://apic.rafflebox.ca/event-service/v2/events/' +
+              eventID +
+              '/recent-purchases',
+            function (data) {
+              var purchasers = data.data.data;
+              var count = 1;
+
+              // Loop and format data
+              for (const recentPurchase of purchasers) {
+                if (
+                  recentPurchase.date &&
+                  recentPurchase.name &&
+                  recentPurchase.totalDollars
+                ) {
+                  var purchased = null;
+                  purchased = new Date(recentPurchase.date);
+                  purchased = timeSince(purchased);
+
+                  // Inject data
+                  jQuery('#recent-' + count + '-name').text(
+                    recentPurchase.name
+                  );
+                  jQuery('#recent-' + count + '-value').text(
+                    '$' + recentPurchase.totalDollars
+                  );
+                  jQuery('#recent-' + count + '-date').text(purchased);
+
+                  count++;
+                }
+              }
+            }
+          );
+        }
+      });
+  }
+
+  // Interval constants for time display
+  const intervals = [
+    { label: 'year', seconds: 31536000 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'day', seconds: 86400 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 },
+  ];
+
+  // Basic natural language time reporting
+  function timeSince(date) {
+    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+    const interval = intervals.find((i) => i.seconds <= seconds);
+    const count =
+      interval.seconds >= 60 ? Math.floor(seconds / interval.seconds) : 0;
+    return count > 0
+      ? `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`
+      : 'moments ago';
+  }
+});
