cdcccdb05b3bcdd05992313dc06b22e680459076
Francis <50196946+Basharfrancis@users.noreply.github.com>
2023-02-16 12:54:23 -0400

    fix linter

--- rb-event-data.js
+++ rb-event-data.js
@@ -3,13 +3,13 @@ $.noConflict();
 jQuery(document).ready(function () {
   // Configuration
   var eventID;
-  var raffleShortlink = "gralaska";
+  var raffleShortlink = 'gralaska';
 
   if (raffleShortlink) {
     // Retrieve Event information
     jQuery
       .get(
-        "https://apic.rafflebox.ca/event-service/v2/events/" + raffleShortlink,
+        'https://apic.rafflebox.ca/event-service/v2/events/' + raffleShortlink,
         function (data) {
           // Get JSON
           var event = data.data.data;
@@ -17,32 +17,32 @@ jQuery(document).ready(function () {
           // Format data
           eventID = event.id;
           var eventName = event.name;
-          var jackpot = "$" + event.displayJackpotCents / 100;
-          var jackpotHalf = "$" + event.displayJackpotCents / 100 / 2;
-          var drawDate = new Date(event.drawDate).toLocaleDateString("en-US", {
-            dateStyle: "long",
+          var jackpot = '$' + event.displayJackpotCents / 100;
+          var jackpotHalf = '$' + event.displayJackpotCents / 100 / 2;
+          var drawDate = new Date(event.drawDate).toLocaleDateString('en-US', {
+            dateStyle: 'long',
           });
           var checkoutLink =
-            "https://checkout.rafflebox.us/goldrush?eventId=" +
+            'https://checkout.rafflebox.us/goldrush?eventId=' +
             eventID +
-            "&locale=en";
+            '&locale=en';
 
           // Inject data
-          jQuery("#eventName").text(eventName);
-          jQuery("#jackpot-total").text(jackpot);
-          jQuery("#jackpot-half").text(jackpotHalf);
-          jQuery("#draw-date").text(drawDate);
-          jQuery("#eventId").text(eventID);
-          jQuery(".rb-checkout").attr("href", checkoutLink);
+          jQuery('#eventName').text(eventName);
+          jQuery('#jackpot-total').text(jackpot);
+          jQuery('#jackpot-half').text(jackpotHalf);
+          jQuery('#draw-date').text(drawDate);
+          jQuery('#eventId').text(eventID);
+          jQuery('.rb-checkout').attr('href', checkoutLink);
         }
       )
       // Then retrieve Recent Purchasers
       .done(function (data) {
         if (eventID) {
           jQuery.get(
-            "https://apic.rafflebox.ca/event-service/v2/events/" +
+            'https://apic.rafflebox.ca/event-service/v2/events/' +
               eventID +
-              "/recent-purchases",
+              '/recent-purchases',
             function (data) {
               var purchasers = data.data.data;
               var count = 1;
@@ -59,13 +59,13 @@ jQuery(document).ready(function () {
                   purchased = timeSince(purchased);
 
                   // Inject data
-                  jQuery("#recent-" + count + "-name").text(
+                  jQuery('#recent-' + count + '-name').text(
                     recentPurchase.name
                   );
-                  jQuery("#recent-" + count + "-value").text(
-                    "$" + recentPurchase.totalDollars
+                  jQuery('#recent-' + count + '-value').text(
+                    '$' + recentPurchase.totalDollars
                   );
-                  jQuery("#recent-" + count + "-date").text(purchased);
+                  jQuery('#recent-' + count + '-date').text(purchased);
 
                   count++;
                 }
@@ -78,12 +78,12 @@ jQuery(document).ready(function () {
 
   // Interval constants for time display
   const intervals = [
-    { label: "year", seconds: 31536000 },
-    { label: "month", seconds: 2592000 },
-    { label: "day", seconds: 86400 },
-    { label: "hour", seconds: 3600 },
-    { label: "minute", seconds: 60 },
-    { label: "second", seconds: 1 },
+    { label: 'year', seconds: 31536000 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'day', seconds: 86400 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 },
   ];
 
   // Basic natural language time reporting
@@ -93,7 +93,7 @@ jQuery(document).ready(function () {
     const count =
       interval.seconds >= 60 ? Math.floor(seconds / interval.seconds) : 0;
     return count > 0
-      ? `${count} ${interval.label}${count !== 1 ? "s" : ""} ago`
-      : "moments ago";
+      ? `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`
+      : 'moments ago';
   }
 });
